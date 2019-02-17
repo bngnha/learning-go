d2651c8487b12b2b56a1587f0e8510c9cf4c1795
Nha Bui <bngnha@gmail.com>
2019-02-17 17:00:43 +0000

    update downloading...

--- videos/youtube.go
+++ videos/youtube.go
@@ -9,24 +9,57 @@ import (
 
 // DlYt function
 func DlYt() {
-	vid, err := ytdl.GetVideoInfo("https://www.youtube.com/watch?v=BzYpUt1PFFI")
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	info, err := ytdl.GetVideoInfo("https://www.youtube.com/watch?v=BzYpUt1PFFI")
 	if err != nil {
 		err = fmt.Errorf("Unable to fetch video info: %s", err.Error())
 		return
 	}
+	formats := info.Formats
+	filters := []string{
+		fmt.Sprintf("%s:mp4", ytdl.FormatExtensionKey),
+		fmt.Sprintf("!%s:", ytdl.FormatVideoEncodingKey),
+		fmt.Sprintf("!%s:", ytdl.FormatAudioEncodingKey),
+		fmt.Sprint("best"),
+	}
 
-	file, _ := os.Create(vid.Title + ".mp4")
+	for _, filter := range filters {
+		filter, err := parseFilter(filter)
+		if err == nil {
+			formats = filter(formats)
+		}
+	}
+
+	var fileName string
+	fileName, err = createFileName("testing."+formats[0].Extension, outputFileName{
+		Title:         sanitizeFileNamePart(info.Title),
+		Ext:           sanitizeFileNamePart(formats[0].Extension),
+		DatePublished: sanitizeFileNamePart(info.DatePublished.Format("2006-01-02")),
+		Resolution:    sanitizeFileNamePart(formats[0].Resolution),
+		Author:        sanitizeFileNamePart(info.Author),
+		Duration:      sanitizeFileNamePart(info.Duration.String()),
+	})
+	if err != nil {
+		err = fmt.Errorf("Unable to parse output file file name: %s", err.Error())
+		return
+	}
+
+	var file *os.File
+	file, err = os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		err = fmt.Errorf("Unable to open output file: %s", err.Error())
+		return
+	}
 	defer file.Close()
-	err = vid.Download(newFormat(vid.Formats[0]), file)
+
+	err = info.Download(formats[0], file)
 	if err != nil {
 		println(err)
 	}
-}
-
-func newFormat(itag int) (Format, bool) {
-	if f, ok := FORMATS[itag]; ok {
-		f.meta = make(map[string]interface{})
-		return f, true
-	}
-	return Format{}, false
 }
