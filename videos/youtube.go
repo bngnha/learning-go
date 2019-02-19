037d6902510fe287b18139964580b04e9a61d64d
Nha Bui <bngnha@gmail.com>
2019-02-19 11:39:10 +0000

    update code...

--- videos/youtube.go
+++ videos/youtube.go
@@ -1,13 +1,13 @@
 package videos
 
 import (
+	"flag"
 	"fmt"
-	"os",
-	"flag",
-	"log",
-	"strings",
+	"log"
+	"os"
+	"strings"
 
-	"github.com/rylio/ytdl",
+	"github.com/rylio/ytdl"
 	"google.golang.org/api/youtube/v3"
 )
 
@@ -77,32 +77,33 @@ var (
 	privacy     = flag.String("privacy", "unlisted", "Video privacy status")
 )
 
+// UlYt function
 func UlYt() {
 	flag.Parse()
 
-	if *filename == "" {
-			log.Fatalf("You must provide a filename of a video file to upload")
-	}
+	//if *filename == "" {
+	//	log.Fatalf("You must provide a filename of a video file to upload")
+	//}
 
 	client := getClient(youtube.YoutubeUploadScope)
 
 	service, err := youtube.New(client)
 	if err != nil {
-			log.Fatalf("Error creating YouTube client: %v", err)
+		log.Fatalf("Error creating YouTube client: %v", err)
 	}
 
 	upload := &youtube.Video{
-			Snippet: &youtube.VideoSnippet{
-					Title:       *title,
-					Description: *description,
-					CategoryId:  *category,
-			},
-			Status: &youtube.VideoStatus{PrivacyStatus: *privacy},
+		Snippet: &youtube.VideoSnippet{
+			Title:       *title,
+			Description: *description,
+			CategoryId:  *category,
+		},
+		Status: &youtube.VideoStatus{PrivacyStatus: *privacy},
 	}
 
 	// The API returns a 400 Bad Request response if tags is an empty string.
 	if strings.Trim(*keywords, "") != "" {
-			upload.Snippet.Tags = strings.Split(*keywords, ",")
+		upload.Snippet.Tags = strings.Split(*keywords, ",")
 	}
 
 	call := service.Videos.Insert("snippet,status", upload)
@@ -110,10 +111,14 @@ func UlYt() {
 	file, err := os.Open(*filename)
 	defer file.Close()
 	if err != nil {
-			log.Fatalf("Error opening %v: %v", *filename, err)
+		log.Fatalf("Error opening %v: %v", *filename, err)
 	}
 
 	response, err := call.Media(file).Do()
 	handleError(err, "")
 	fmt.Printf("Upload successful! Video ID: %v\n", response.Id)
+}
+
+func handleError(err error, msg string) {
+	fmt.Println(msg)
 }
